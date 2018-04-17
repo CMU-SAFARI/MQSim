d5ea156719cadbf2e75bf181305cb44d11a73e16
Arash Tavakkol <arash.tavakkol@inf.ethz.ch>
2018-04-17 16:47:53 +0200

    Remove debug code

--- src/sim/Sim_Defs.h
+++ src/sim/Sim_Defs.h
@@ -24,7 +24,7 @@ typedef std::string sim_object_id_type;
 						 }
 #define PRINT_MESSAGE(M) std::cout << M << std::endl;
 #define DEBUG(M) //std::cout<<M<<std::endl;
-#define DEBUG2(M) std::cout<<M<<std::endl;
+#define DEBUG2(M) //std::cout<<M<<std::endl;
 #define SIM_TIME_TO_MICROSECONDS_COEFF 1000
 #define SIM_TIME_TO_SECONDS_COEFF 1000000000
 #endif // !DEFINITIONS_H
