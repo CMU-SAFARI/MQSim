885136bf4e7ef4b9af65e4174d3f81dd25250326
Timon Evenblij <Timon.Evenblij@imec.be>
2019-03-04 14:24:40 +0100

    Added press any key message at the end

--- src/main.cpp
+++ src/main.cpp
@@ -330,7 +330,7 @@ int main(int argc, char* argv[])
 		PRINT_MESSAGE("Writing results to output file .......");
 		collect_results(ssd, host, (workload_defs_file_path.substr(0, workload_defs_file_path.find_last_of(".")) + "_scenario_" + std::to_string(cntr) + ".xml").c_str());
 	}
-
+    cout << "Simulation complete; Press any key to exit." << endl;
 	cin.get();
 	return 0;
 }
