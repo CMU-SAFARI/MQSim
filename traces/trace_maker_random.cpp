15fe6e313efc0922bf7869675902df543a2ba870
Nae Young song <nai0315@gmail.com>
2021-02-01 21:28:47 -0600

    modified trace_maker_random.cpp

--- traces/trace_maker_random.cpp
+++ traces/trace_maker_random.cpp
@@ -6,26 +6,27 @@
 
 using namespace std;
 
-unsigned int sector_size_in_bytes = 512;
-unsigned int zone_size_MB = 256;
+const unsigned int sector_size_in_bytes = 512;
+const unsigned int zone_size_MB = 256;
 unsigned int smallest_zone_number = 2;
-unsigned int biggest_zone_number = 6;
+unsigned int biggest_zone_number;
+unsigned int total_size_of_accessed_file_in_GB = 1;
 unsigned int total_no_of_request = 10000;
-
-unsigned int zone_number = biggest_zone_number - smallest_zone_number + 1;
+unsigned int zone_number;
 
 struct zone {
+    unsigned int zone_ID;
     unsigned long long int start_LBA;
     unsigned long long int writepoint_LBA;
     bool available;
 };
 
-struct zone *zonelist = (struct zone*)malloc(sizeof(struct zone) * (zone_number + 1));
+struct zone *zonelist;
 
 bool all_zonelist_false()
 {
-    for (int i = 0; i < zone_number; i++) {
-        if(zonelist[i].available == true)
+    for (int k = 0; k < zone_number; k++) {
+        if(zonelist[k].available == true)
             return false;
     }
     return true;   
@@ -35,17 +36,30 @@ int main(int argc, char** argv) {
 
     std::cout << "This generates a random write requests." << std::endl;
 
-    if (argc < 3) {
-        cout << "please, name the out file, \"./a.out <request size in KB> <output file name>\"" << endl;
-        return 1;
-    }
-    string outputfile = argv[2];
-    string request_size = argv[1];
+    unsigned int request_size_in_KB;
+    cout << "Please enter the request size in KB : ";
+    scanf("%u", &request_size_in_KB);
+
+    cout << "Please enter the smallest zone number : ";
+    scanf("%u", &smallest_zone_number);
+
+    cout << "Please enter the whole file size to be accessed in GB : ";
+    scanf("%u", &total_size_of_accessed_file_in_GB);
+
+    string outputfile = "random_write_" + to_string(request_size_in_KB) + "KB_from_zone" \
+                        + to_string(smallest_zone_number) + "_" + to_string(total_size_of_accessed_file_in_GB) + "GB";
+    cout << "output file name is " << outputfile << endl;
+    
+    zone_number = total_size_of_accessed_file_in_GB * 1024 / 256;
+    cout << "We are gonna access " << zone_number << " zones" << endl;
+
+    zonelist = (struct zone*)malloc(sizeof(struct zone) * (zone_number+1));
 
     // zonelist initialization
     for (int i = 0; i < zone_number + 1; i++) {
-        zonelist[i].start_LBA = (smallest_zone_number + i) * 1024 * 1024 * 512; // 256 MB / 512 B
-        cout << "each zone's start_LBA: " << zonelist[i].start_LBA << endl;
+        zonelist[i].zone_ID = smallest_zone_number + i;
+        zonelist[i].start_LBA = zonelist[i].zone_ID * 1024 * 512; // 256 MB / 512 B
+        cout << "zone" << zonelist[i].zone_ID << "'s start_LBA: " << zonelist[i].start_LBA << endl;
         zonelist[i].writepoint_LBA = zonelist[i].start_LBA;
         zonelist[i].available = true;
     }
@@ -58,9 +72,11 @@ int main(int argc, char** argv) {
     writeFile.open(outputfile);
     string trace_line = "";
 
-    unsigned int request_size_in_KB = stoi(request_size);
-    cout << "request size is " << request_size_in_KB << " KB" << endl;
-    unsigned int first_arrival_time = 48513000;
+   // cout << "request size is " << request_size_in_KB << " KB" << endl;
+    total_no_of_request =  total_size_of_accessed_file_in_GB * 1024 * 1024 / request_size_in_KB;
+    cout << "total number of requests is " << total_no_of_request << endl;
+
+    unsigned long long int first_arrival_time = 4851300;
 
     string arrival_time;
     string start_LBA;
@@ -68,25 +84,24 @@ int main(int argc, char** argv) {
     string request_size_in_sector;
     string type_of_request;
 
-    unsigned int prev_arrival_time = first_arrival_time;
+    unsigned long long int prev_arrival_time = first_arrival_time;
     unsigned long long int prev_start_LBA;
     int i, j;
-    for (i = 0; i < total_no_of_request; i++) {
-        int zone_id = rand() % zone_number;
-        // while (zone_id = rand() % zone_number + smallest_zone_number) {
-        //     if(zonelist[zone_id].available == true)
-        //         break; 
-        //     else if (all_zonelist_false()) {
-        //         cout << "Every zone is filled up" << endl;
-        //         writeFile.close();
-        //         exit(0);
-        //     }
-        // }
+    int zone_id;
+    for (i = 0; i < total_no_of_request + 1000000; i++) {
+        do {
+            if (all_zonelist_false()) {
+                cout << "Every zone is filled up!" << endl;
+                writeFile.close();
+                return 0;
+            }
+            zone_id = rand() % zone_number;
+        } while (zonelist[zone_id].available == false);
 
         int access_count_in_one_zone = rand() % 50;
         prev_start_LBA = zonelist[zone_id].writepoint_LBA;
 
-        for (j = 0; j < access_count_in_one_zone; j++) {
+        for (j = 0; j < access_count_in_one_zone; j++, i++) {
             arrival_time = to_string(prev_arrival_time);
             start_LBA = to_string(prev_start_LBA);
             device_number = "1";
@@ -98,17 +113,14 @@ int main(int argc, char** argv) {
             writeFile.write(trace_line.c_str(), trace_line.size());
             trace_line.clear();
             
-            sscanf(arrival_time.c_str(), "%d", &prev_arrival_time); 
-            sscanf(start_LBA.c_str(), "%llu", &prev_start_LBA); 
             prev_arrival_time = prev_arrival_time + ((rand() % 15) * 1000);
             prev_start_LBA = prev_start_LBA + (request_size_in_KB * 2); // 2 == 1024 / sector_size_in_bytes
             zonelist[zone_id].writepoint_LBA = prev_start_LBA;
             if (zonelist[zone_id].writepoint_LBA >= zonelist[zone_id+1].start_LBA) {
                 zonelist[zone_id].available = false;
-                cout << "zone # " << zone_id << "is filled up! no more write request!!" << endl;
+                cout << "zone #" << zonelist[zone_id].zone_ID << " is filled up! no more write request!!" << endl;
                 break;
             }
-            i++;
         }
     }
     writeFile.close();
